e67d7ef6fa7c1ccad352ffac51a0e153f53c73e8
Bunyod Rafikov <bunyod.rafikov@crunchfish.com>
2025-01-04 22:19:22 +0100

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Bunyod Rafikov on 2025-01-04.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
